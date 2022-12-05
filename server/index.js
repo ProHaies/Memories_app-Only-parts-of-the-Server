e5f582228299d230f51ac498f0ab564d626dee0a
Borislav Stefanov <prohaies3101@gmail.com>
2022-12-05 18:18:31 +0200

    first commit

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import cors from "cors"
 import dotenv from 'dotenv'
 import postRoutes from './routes/posts.js'
+import userRoutes from './routes/users.js'
 
 const app = express();
 dotenv.config()
@@ -12,6 +13,7 @@ app.use(bodyParser.urlencoded({limit:"30mb", extended:true}))
 app.use(cors());
 app.use('/posts', postRoutes)
 const PORT = process.env.PORT || 5000;
+app.use('/user', userRoutes)
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
